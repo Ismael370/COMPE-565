dfabcfa86cf9d24226ff24455141aeb1cdcc5d35
Ismael Chavez <chavez.ismael7@gmail.com>
2022-04-28 12:20:24 -0700

    Finalized README

--- HW4/README.m
+++ HW4/README.m
@@ -9,17 +9,19 @@ clc, clear
 
 video = VideoReader('football_qcif.avi');
 
-for i = 10:13
-    ref_frame = read(video,i);
-    curr_frame = read(video, i+1);
-    
-    ref_frame = rgb2ycbcr(ref_frame);
+y_ref = zeros(144, 176);
+
+for i = 10:14
+%     ref_frame = read(video,i);
+    curr_frame = read(video, i);
+%     
+%     ref_frame = rgb2ycbcr(ref_frame);
     curr_frame = rgb2ycbcr(curr_frame);
     
     %%%Separate the ycbcr channels in 4:2:0
-    y_ref = ref_frame(:,:,1);
-    cb_ref_sub = ref_frame(1:2:144,1:2:176, 2);
-    cr_ref_sub = ref_frame(1:2:144,1:2:176, 3);
+%     y_ref = ref_frame(:,:,1);
+%     cb_ref_sub = ref_frame(1:2:144,1:2:176, 2);
+%     cr_ref_sub = ref_frame(1:2:144,1:2:176, 3);
     
     y_curr = curr_frame(:,:,1);
     cb_curr_sub = curr_frame(1:2:144,1:2:176, 2);
@@ -27,67 +29,62 @@ for i = 10:13
     
     y_predicted = uint8(zeros(144, 176));
     
-    %create array to hold motion vectors of this frame and empty
-    motionVectors = zeros(1,4);
-    motionVectors(:,:) = [];
-    
-    %%%Iterate through the macroblocks
-    for mb_row = 16:16:144
-        for mb_col = 16:16:176
-            min_sad = 999999;
-            bm_row = 1;% Best macth ending row
-            bm_col = 1; % Best macth ending col
-            mb_curr = y_curr(mb_row-15:mb_row, mb_col-15:mb_col);
-            
-            %%%Search window rows and cols
-            start_row = mb_row-23;
-            end_row = mb_row+8;
-            start_col = mb_col-23;
-            end_col = mb_col+8;
-            
-            %%%Handle edge cases
-            if mb_row == 16
-                start_row = 1;
-            end
-            
-            if mb_row == 144
-                end_row = 144;
-            end
-            
-            if mb_col == 16
-                start_col = 1;
-            end
-            
-            if mb_col == 176
-                end_col = 176;
-            end
-            
-            %%%Search each macroblock in the window
-            for sw_row = start_row+15:end_row
-                for sw_col = start_col+15:end_col
-                    %%%Calculate SAD
-                    mb_ref = y_ref(sw_row-15:sw_row,sw_col-15:sw_col);
-                    sad = abs(sum(sum(mb_ref-mb_curr)));
-                    
-                    if sad < min_sad
-                        min_sad = sad;
-                        bm_row = sw_row;
-                        bm_col = sw_col;
+    if(i > 10)
+        %%%Iterate through the macroblocks
+        for mb_row = 16:16:144
+            for mb_col = 16:16:176
+                min_sad = 999999;
+                bm_row = 1;% Best macth ending row
+                bm_col = 1; % Best macth ending col
+                mb_curr = y_curr(mb_row-15:mb_row, mb_col-15:mb_col);
+
+                %%%Search window rows and cols
+                start_row = mb_row-23;
+                end_row = mb_row+8;
+                start_col = mb_col-23;
+                end_col = mb_col+8;
+
+                %%%Handle edge cases
+                if mb_row == 16
+                    start_row = 1;
+                end
+
+                if mb_row == 144
+                    end_row = 144;
+                end
+
+                if mb_col == 16
+                    start_col = 1;
+                end
+
+                if mb_col == 176
+                    end_col = 176;
+                end
+
+                %%%Search each macroblock in the window
+                for sw_row = start_row+15:end_row
+                    for sw_col = start_col+15:end_col
+                        %%%Calculate SAD
+                        mb_ref = y_ref(sw_row-15:sw_row,sw_col-15:sw_col);
+                        sad = abs(sum(sum(mb_ref-mb_curr)));
+
+                        if sad < min_sad
+                            min_sad = sad;
+                            bm_row = sw_row;
+                            bm_col = sw_col;
+                        end
                     end
                 end
+
+                %%%Reconstruct predicted frame
+                y_predicted(mb_row-15:mb_row, mb_col-15:mb_col) = y_ref(bm_row-15:bm_row, ...
+                    bm_col-15:bm_col); 
             end
-            
-            %append reference point and each motion vector 
-            motionVectors = [motionVectors;mb_row,mb_col,bm_row,bm_col];
-            
-            %%%Reconstruct predicted frame
-            y_predicted(mb_row-15:mb_row, mb_col-15:mb_col) = y_ref(bm_row-15:bm_row, ...
-                bm_col-15:bm_col); 
         end
+
+        %%%Calculate difference frame
+        y_diff = y_curr - y_predicted;
     end
-    
-    %%%Calculate difference frame
-    y_diff = y_curr - y_predicted;
         
     %%%DCT on difference frame
     y_diff_decoded = zeros(144, 176);
@@ -100,8 +97,14 @@ for i = 10:13
             % Encoder
             % M-file name: dct.m, README.m
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            y_dct = dct(y_diff(x:x+7, y:y+7));
+            if(i > 10)
+                y_dct = dct(y_diff(x:x+7, y:y+7));
+            else
+                y_dct = dct(y_curr(x:x+7, y:y+7));
+            end
+            
             y_quant = round(y_dct/28);
+ 
             
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % Decoder
@@ -114,7 +117,11 @@ for i = 10:13
         end
     end
     
-    y_decoded = uint8(y_diff_decoded) + y_predicted;
+    if(i > 10)
+        y_decoded = uint8(y_diff_decoded) + y_predicted;
+    else
+        y_decoded = uint8(y_diff_decoded);
+    end
     
     %%%DCT on Cb and Cr channels
     cb_sub_decoded = zeros(72, 88);
@@ -150,6 +157,9 @@ for i = 10:13
         end
     end
     
+    %%%Store frame
+    y_ref = y_curr;
+    
     %%%Upsample Cb and Cr using replication
     cb_decoded(1:2:144, 1:2:176) = cb_sub_decoded(:, :);
     cb_decoded(2:2:144, :) = cb_decoded(1:2:144, :);
@@ -160,7 +170,7 @@ for i = 10:13
     cr_decoded(2:2:144, :) = cr_decoded(1:2:144, :);
     cr_decoded(:, 2:2:176) = cr_decoded(:, 1:2:176);
     cr_decoded = cr_decoded;
-    
+        
     %%%Get RGB frames
     ycbcr_decoded = cat(3, y_decoded, cb_decoded, cr_decoded);
     rgb_decoded = ycbcr2rgb(ycbcr_decoded);
